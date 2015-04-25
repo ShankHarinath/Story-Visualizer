0444bce4c713ac4fe6e84099f255233c8de876bd
Ankita Nargundkar <nargundk@usc.edu>
2015-04-25 02:29:00 -0700

    third

--- InformationExtraction.py
+++ InformationExtraction.py
@@ -7,86 +7,107 @@ import pickle
 import sys
 import nltk
 from nltk import *
+from nltk.tag.stanford import NERTagger
 
-def BifurcateInformation(element,elementName):
+def BifurcateInformation(elements,elementName):
 	elementMap = {}
-	PersonList = []
-	OrganisationList = []
-	LocationList = []
+	Person = set()
+	Organisation = set()
+	Location= set()
+
+	list1=[]
+	word = ""
 	
 	
-	for i,w in enumerate(element):
-		NERtag = element[i][1]
-		if NERtag == "B-PER" :
-			word = element[i][0]
-			if i < len(element) -2:
-				if element[i+1][1] == "I-PER":
-					i+=1
-					while element[i][1] == "I-PER": 
-						word = word +" "+element[i][0]
-						i+=1
-						
-			if word not in PersonList:
-				PersonList.append(word)
-				
-		
-		if NERtag == "B-ORG" :
-			word = element[i][0]
-			if i < len(element) -2:
-				if element[i+1][1] == "I-ORG":
-					i+=1
-					while element[i][1] == "I-ORG": 
-						word = word +" "+element[i][0]
-						i+=1
-	
-			if word not in OrganisationList:
-				OrganisationList.append(word)
-				
-		
+	for i,element in enumerate(elements):
+		NERtag = element[1]
+		if NERtag == "PERSON":
+			word = element[0]
+			i+=1
+			while i < len(elements)-2 and elements[i][1] == "PERSON": 
+				word = word +" "+elements[i][0]
+				#print("adding "+ word)
+				i+=1
 			
-		
-		if NERtag == "B-LOC" :
-			word = element[i][0]
-			if i < len(element) -2:
-				if element[i+1][1] == "I-LOC":
-					i+=1
-					while element[i][1] == "I-LOC": 
-						word = word +" "+element[i][0]
-						i+=1
-			if word not in LocationList:
-				LocationList.append(word)
-				
-		
+			Name = word.split()
+			for words in Person:
+				a = words.split()
+				for b in a:
+					list1.append(b)
+			intersection = [val for val in Name if val in list1]
+			if len(intersection) == 0: 
+				Person.add(word)
+			word = ""
+			list1 = []
+
+		if NERtag == "ORGANIZATION":
+			word = element[0]
+			i+=1
+			while i < len(elements)-2 and elements[i][1] == "ORGANIZATION": 
+				word = word +" "+elements[i][0]
+				#print("adding "+ word)
+				i+=1
 			
-	elementMap["Person"] = PersonList
-	elementMap["Organisation"] = OrganisationList
-	elementMap["Location"] = LocationList
+			Name = word.split()
+			for words in Organisation:
+				a = words.split()
+				for b in a:
+					list1.append(b)
+			intersection = [val for val in Name if val in list1]
+			if len(intersection) == 0: 
+				Organisation.add(word)
+			word = ""
+			list1 = []
+			
+		if NERtag == "LOCATION":
+			word = element[0]
+			i+=1
+			while i < len(elements)-2 and elements[i][1] == "LOCATION": 
+				word = word +" "+elements[i][0]
+				#print("adding "+ word)
+				i+=1
+			
+			Name = word.split()
+			for words in Location:
+				a = words.split()
+				for b in a:
+					list1.append(b)
+			intersection = [val for val in Name if val in list1]
+			if len(intersection) == 0: 
+				Location.add(word)
+			word = ""
+			list1 = []
+	
+	elementMap["Person"] = Person
+	elementMap["Organisation"] = Organisation
+	elementMap["Location"] = Location
 	print(elementName)
 	print(elementMap)
 	elementMap.clear()
 	PersonList = []
 	OrganisationList = []
 	LocationList = []
-	
+
 	
 				
 
 def GenerateSubjectRelationObject(lineWithTags,subject,action,object):
-	subjectList = subject[0].split()
-	actionList = action[0].split()
-	objectList = object[0].split()
-	
+	subjectList = subject.split()
+	actionList = action.split()
+	objectList = object.split()
+
 	intermediateSubjectList = []
 	intermediateActionList = []
 	intermediateObjectList = []
 	
-	for token in lineWithTags[2:len(lineWithTags)-2]:
-		
-		if token[0] in subjectList:
+	
+
+	for token in lineWithTags:
+		if token[0] in subjectList and [token[0],token[1]] not in intermediateSubjectList:
 			intermediateSubjectList.append([token[0],token[1]])
-		if token[0] in actionList:
+		if token[0] in actionList and [token[0],token[1]] not in intermediateActionList:
 			intermediateActionList.append([token[0],token[1]])
-		if token[0] in objectList:
+		if token[0] in objectList and [token[0],token[1]] not in intermediateObjectList:
 			intermediateObjectList.append([token[0],token[1]])
 	
 	
@@ -102,7 +123,7 @@ def GenerateSubjectRelationObject(lineWithTags,subject,action,object):
 	
 	
 relations = pickle.load(open(sys.argv[1],"rb"))
-ner_tagger = SennaNERTagger('C:\Users\Ankita\eclipse_workspace\StoryTeller\storyteller\senna')
+ner_tagger = NERTagger('Java/english.muc.7class.nodistsim.crf.ser.gz', 'Java/stanford-ner-3.5.2.jar')
 
 sentence =""
 subject=""
@@ -110,20 +131,17 @@ action = ""
 object = ""
 for relation in relations:
 	sentence = ""
-	subject = relation[0]
-	action = relation[1]
-	object = relation[2]
+	subject = relation[0][0]
+	action = relation[1][0]
+	object = relation[2][0]
+	
 	for token in relation:
 		for word in token:
 			sentence = sentence +" "+ word
-
-	sentences1 = tokenize.sent_tokenize(sentence)
-	taggedLine = ner_tagger.tag(word_tokenize(str(sentences1)))
-	print(sentences1)
-	print(sentence)
-	print(taggedLine
-	)
-	GenerateSubjectRelationObject(taggedLine,subject,action,object)	
-
+	
+	taggedLine = ner_tagger.tag(word_tokenize(sentence))
+	print(taggedLine)
+	GenerateSubjectRelationObject(taggedLine[0],subject,action,object)	
+	
 	
 	
